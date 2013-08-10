dcbd0b79c962c0b134da3ff5a53fd525a65f49dd
Stephen Finucane <stephenfinucane@hotmail.com>
2013-08-10 13:30:49 +0100

    Initial implementation of datasources

--- ul-rest-api/handlers.py
+++ ul-rest-api/handlers.py
@@ -1,230 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2013, Stephen Finucane
+
+# Author: Stephen Finucane <stephenfinucane@hotmail.com>
+
+""" handlers.py: Handlers for all URIs in application """
+
+import collections
 import webapp2
 import json
 
+import datasources.course as course
+import datasources.geolocation as geolocation
+import datasources.scheduling as scheduling
+import datasources.staff as staff
+
 from datetime import datetime
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+class DocumentationHandler(webapp2.RequestHandler):
+  """ Documentation Services """
+  def get(self):
+    #Parse the URI
+    self.response.write("Documentation") 
 
 '''
 Handles the following services:
-	Scheduling:
-		academic_calendar	Academic Calendar
-		exam_timetable 		Exam Timetables
-		semester_timetable	Semester Timetables
-	Geolocation:
-		building			List all buildings on campus or parse a building code
-		room				Parse a room code
-	Course:
-		course 				Provides a brief overview of a course
-		module 				Provides a brief overview of a module
-	Staff:
-		staff 				Provides a brief overview of a staff member
+  Scheduling:
+    calendar            Academic Calendar
+    timetable           Semester Timetables
+    exam_timetable      Exam Timetables
 '''
+class SchedulingHandler(webapp2.RequestHandler):
+  """ Scheduling Services """
+  def get(self):
+    #Parse the URI
+    self.response.write("Scheduling") 
 
-class RestHandler(webapp2.RequestHandler):
-	
-	""" Main methods """
+    #Parse the parameters from the URL
+    query = self.request.get('q')
 
-	def get(self):
-		#Parse the parameters from the URL
-		service_id = self.request.get('service')
+    if not query:
+      self.generate_error('Please include the parameter \'q\'')
+      return
 
-		if not service_id:
-			self.error(404)
-			return
+    result = {
+    '/api/v1/timetable' : self.get_timetable,
+    '/api/v1/exam_timetable' : self.get_exam_timetable,
+    '/api/v1/calendar' : self.get_calendar,
+    }.get(self.request.path, self.error)(query)
 
-		services = {
-			'academic_calendar' : self.academic_calendar,
-			'exam_timetable' : self.exam_timetable,
-			'timetable' : self.semester_timetable,
-			'building' : self.building_search,
-			'room' : self.room_search,
-			'course' : self.course_search,
-			'module' : self.module_search,
-			'staff' : self.staff_search
-		}
+    self.response.write(json.dumps(result))
 
-		services.get(service_id,self.generate_error)()
+  '''
+  Retrieve the timetable for a given ID and return the result 
+  '''
+  def get_timetable(self, query):
+    # Retrieve a timeable for a given ID number (query)
+    data = scheduling.semester_timetable(query)
+    
+    if not data:
+      return -1
 
-	def generate_error(self, errors = 'Required parameters invalid'):
-		response = { 
-			'errors' : errors
-		}
-		self.response.write(json.dumps(response))
+    result = collections.OrderedDict()
+    result['id'] = query
+    result['date_created'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    result['data'] = data
 
-	def generate_output(self, message):
-		header = {
-			'date_created' : datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		}
-		response = dict(header.items() + message.items())
-		self.response.write(json.dumps(response))
+    return result
 
+  def get_exam_timetable(self, query):
+    self.response.write("Error")
 
-	""" Scheduling Services """
+  def get_calendar(self, query):
+    # Retrieve a calendar for a given year (query)
+    data = scheduling.calendar(query)
+    
+    if not data:
+      return -1
 
-	'''
-	List details of a given academic calendar. Alternatively, list details of 
-	current academic calendar
-	'''
-	def academic_calendar(self):
-		#Parse the parameters from the URL
-		calendar_year = self.request.get('q')
+    result = collections.OrderedDict()
+    result['id'] = query
+    result['date_created'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    result['data'] = data
 
-		# TODO: return all calendar details
+    return result
 
-		message = {
-			'results' : 'Hello World from academic calendar service'
-		}
-		self.generate_output(message)
+  def error(self, query):
+    self.response.write("Error")
 
-	'''
-	List details of a given student's exam timetable for the current semester
-	'''
-	def exam_timetable(self):
-		#Parse the parameters from the URL
-		student_id = self.request.get('q')
+'''
+Handles the following services:
+  Geolocation:
+    building            List all buildings on campus or parse a building code
+    room                Parse a room code
+'''
+class GeolocationHandler(webapp2.RequestHandler):
+  """ Geolocation Services """
+  def get(self):
+    #Parse the URI
+    self.response.write("Geolocation") 
 
-		if not student_id:
-			self.generate_error('Please include the parameter "q"')
-			return
+'''
+Handles the following services:
+  Course:
+    course              Provides a brief overview of a course
+    module              Provides a brief overview of a module
+'''
+class CourseHandler(webapp2.RequestHandler):
+  """ Course Services """
+  def get(self):
+    #Parse the URI
+    self.response.write("Course") 
 
-		# TODO: retrieve the timetable
-
-		message = {
-			'query' : student_id,
-			'timetable' : 'Hello World from exam timetable service'
-		}
-		self.generate_output(message)
-
-	'''
-	List details of a given student's class timetable for the current semester
-	'''
-	def semester_timetable(self):
-		#Parse the parameters from the URL
-		student_id = self.request.get('q')
-
-		if not student_id:
-			self.generate_error('Please include the parameter \'q\'')
-			return
-
-		# TODO: retrieve the timetable
-
-		message = {
-			'query' : student_id,
-			'timetable' : 'Hello World from semester timetable service'
-		}
-		self.generate_output(message)
-
-
-	""" Geo-location Services """
-
-	'''
-	List details of a given building. Alternatively, list all buildings on 
-	campus.
-	'''
-	def building_search(self):
-		#Parse the parameters from the URL
-		building_code = self.request.get('q')
-
-		if not building_code:
-			pass # TODO: return all buildings on campus
-		else:
-			pass # TODO: parse and retrieve details for the given room code
-
-		message = {
-			'query' : building_code,
-			'results' : 'Hello World from building search service'
-		}
-		self.generate_output(message)
-
-	'''
-	Provide details for a room (i.e. parse room code)
-	'''
-	def room_search(self):
-		#Parse the parameters from the URL
-		room_code = self.request.get('q')
-
-		if not room_code:
-			self.generate_error('Please include the parameter \'q\'')
-			return
-
-		# TODO: parse and retrieve details for the given room code
-
-		message = {
-			'query' : room_code,
-			'results' : 'Hello World from room search service'
-		}
-		self.generate_output(message)
-
-
-	""" Course Services """
-
-	'''
-	List details of a given course.
-	'''
-	def course_search(self):
-		#Parse the parameters from the URL
-		course_code = self.request.get('q')
-
-		if not course_code:
-			self.generate_error('Please include the parameter \'q\'')
-			return
-
-		# TODO: parse and retrieve details for the given course code
-
-		message = {
-			'query' : course_code,
-			'results' : 'Hello World from course code service'
-		}
-		self.generate_output(message)
-
-	'''
-	List details of a given module.
-	'''
-	def module_search(self):
-		#Parse the parameters from the URL
-		module_code = self.request.get('q')
-
-		if not module_code:
-			self.generate_error('Please include the parameter \'q\'')
-			return
-
-		# TODO: parse and retrieve details for the given module code
-
-		message = {
-			'query' : module_code,
-			'results' : 'Hello World from module code service'
-		}
-		self.generate_output(message)
-
-
-	""" Staff Services """
-
-	'''
-	List details of a given staff member. Alternatively, list all staff members matching the given criteria.
-
-	For people search, use the following links:
-		Maths & Stats: 		http://www.macsi.ul.ie/people.php
-		ECE: 				http://www.ece.ul.ie/index.php/contacts/academic.html
-		KBS: 				http://www.ul.ie/business/about/kbs-staff
-		Physics and Energy: http://www.energy.ul.ie/people/faculty
-		...etc...
-
-	'''
-	def staff_search(self):
-		#Parse the parameters from the URL
-		search_term = self.request.get('q')
-
-		if not search_term:
-			self.generate_error('Please include the parameter \'q\'')
-			return
-
-		# TODO: parse and retrieve details for the given room code
-
-		message = {
-			'query' : room_code,
-			'results' : 'Hello World from staff search service'
-		}
-		self.generate_output(message)
+'''
+Handles the following services:
+  Staff:
+    staff               Provides a brief overview of a staff member
+'''
+class StaffHandler(webapp2.RequestHandler):
+  """ Staff Services """
+  def get(self):
+    #Parse the URI
+    self.response.write("Staff") 
--- ul-rest-api/main.py
+++ ul-rest-api/main.py
@@ -16,8 +16,27 @@
 #
 import handlers
 import webapp2
+from webapp2_extras import routes
 
 app = webapp2.WSGIApplication([
-    ('/', handlers.MainHandler),
-    ('/api/v1/', handlers.RestHandler)
+    routes.PathPrefixRoute('/api/v1', [
+      webapp2.Route('/', handlers.DocumentationHandler,
+        name=""),
+      webapp2.Route('/calendar', handlers.SchedulingHandler, 
+        name="calendar"),
+      webapp2.Route('/timetable', handlers.SchedulingHandler, 
+        name="timetable"),
+      webapp2.Route('/examtimetable', handlers.SchedulingHandler, 
+        name="examtimetable"),
+      webapp2.Route('/building', handlers.GeolocationHandler, 
+        name="building"),
+      webapp2.Route('/room', handlers.GeolocationHandler, 
+        name="room"),
+      webapp2.Route('/course', handlers.CourseHandler, 
+        name="course"),
+      webapp2.Route('/module', handlers.CourseHandler, 
+        name="module"),
+      webapp2.Route('/staff', handlers.StaffHandler, 
+        name="staff"),
+    ]),
 ], debug=True)
